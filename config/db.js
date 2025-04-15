00ca5f21ae973270161a661f4338408cad875b63
Kunal Bhardwaj <kunalrocker62@gmail.com>
2025-04-16 01:45:28 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,10 +3,20 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb://localhost:27017/my_social_media',{
+    const mongoUri = process.env.NODE_ENV === 'production'
+      ? process.env.PROD_MONGO_URI
+      : process.env.DEV_MONGO_URI;
+
+    if (!mongoUri) {
+      throw new Error('MongoDB URI is not defined in environment variables');
+    }
+
+    const conn = await mongoose.connect(mongoUri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
+
+    console.log("Mongo URI:", mongoUri);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
